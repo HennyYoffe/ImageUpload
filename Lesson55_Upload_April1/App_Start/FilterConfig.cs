604270b19d2dda8fd8ed5d71edb77b11afd19530
Henny Yoffe <hennyy1149@gmail.com>
2019-04-01 21:57:40 -0400

    Add project files.

---
+++ Lesson55_Upload_April1/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Lesson55_Upload_April1
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Lesson55_Upload_April1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lesson55_Upload_April1.MvcApplication" Language="C#" %>
